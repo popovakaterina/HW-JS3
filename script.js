ea901e9f7f43ffa8fb38e772cc7e3bab840521e7
Popova Ekaterina <popova_katerina97@mail.ru>
2023-01-18 23:00:48 +0500

    Script for 4&5 tasks

--- script.js
+++ script.js
@@ -52,3 +52,69 @@ Task3But.addEventListener("click", function(){
     const Val3Value = Val3.value;
     max (Val1Value,Val2Value,Val3Value);
 });
+
+// Задание 4
+
+function add (a,b) {
+    return a+b;
+};
+function sub (a,b) {
+  return a-b;
+};
+function mult (a,b) {
+   return a*b;
+};
+function divis (a,b) {
+    return a/b;
+};
+
+const Number1 = document.querySelector(".number1");
+const Number2 = document.querySelector(".number2");
+const Task4But1 = document.querySelector(".Addition");
+const Task4But2 = document.querySelector(".Subtraction");
+const Task4But3 = document.querySelector(".Multiplication");
+const Task4But4 = document.querySelector(".Division");
+
+Task4But1.addEventListener ("click", function(){
+    const Numb1Value = Number(Number1.value);
+    const Numb2Value = Number(Number2.value);
+    alert (`Сумма = ${add(Numb1Value,Numb2Value)}`);
+});
+Task4But2.addEventListener ("click", function(){
+    const Numb1Value = Number(Number1.value);
+    const Numb2Value = Number(Number2.value);
+    alert (`Разность = ${sub(Numb1Value,Numb2Value)}`);
+});
+Task4But3.addEventListener("click",function(){
+    const Numb1Value = Number(Number1.value);
+    const Numb2Value = Number(Number2.value);
+    alert (`Произведения = ${mult(Numb1Value,Numb2Value)}`);
+});
+Task4But4.addEventListener("click",function(){
+    const Numb1Value = Number(Number1.value);
+    const Numb2Value = Number(Number2.value);
+    alert (`Частное = ${divis(Numb1Value,Numb2Value)}`);
+});
+
+//Задание 5
+
+function isPrime(num) {
+    if((num % 2 === 0 && num !== 2) || num <=1) {
+        return alert (`${num} - не является простым числом`);
+    }
+    const limit = Math.floor(Math.sqrt(num));
+    for(let index = 3; index<=limit; index+=2){
+        if (num % index === 0) {
+            return alert (`${num} - не является простым числом`);
+        }
+    }
+    return alert (`${num} - является простым числом`);
+};
+
+const Numb = document.querySelector(".number");
+const Task5But = document.querySelector(".task5But");
+
+Task5But.addEventListener("click", function(){
+    const NumbValue = Number(Numb.value);
+    isPrime(NumbValue);
+});
